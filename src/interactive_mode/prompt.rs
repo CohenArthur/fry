94c279d3ec9061c7249db16d4c477ff1648ce52e
cohenarthur <arthur.cohen@epita.fr>
2020-05-03 01:37:29 +0200

    prompt: Fix format in doc

--- src/interactive_mode/prompt.rs
+++ src/interactive_mode/prompt.rs
@@ -1,5 +1,5 @@
 //! The Prompt is the text showed at the beginning of the line in interactive
-//! mode. It changes based on the context
+//! mode. It changes based on the context.
 
 use colored::Colorize;
 
